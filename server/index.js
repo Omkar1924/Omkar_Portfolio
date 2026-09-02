9fae8ff37087166ba138b9959b2cf978c8e20c39
AdityaDipakTate <omkar2002sh@gmail,com>
2026-09-02 22:04:03 +0530

    cors pfate

--- server/index.js
+++ server/index.js
@@ -7,8 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({ origin: 'https://omkar-portfolio-lovat.vercel.app' })); app.use(express.json());
 
 // Health Check Route
 app.get('/api/health', (req, res) => {
